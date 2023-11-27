dee7de8cbc630e90c7311040c73317a1431a12e6
denisdenis05 <denisgre205@gmail.com>
2023-11-27 21:40:51 +0200

    Initial commit

---
+++ src/main/java/com/spotiInfo/app/SpotiInfoApplication.java
@@ -0,0 +1,13 @@
+package com.spotiInfo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotiInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotiInfoApplication.class, args);
+	}
+
+}
